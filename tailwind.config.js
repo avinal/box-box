f1d6d1f8df80975c60e2ecf8d38e6d187fb710fd
Avinal Kumar <avinal.xlvii@gmail.com>
2025-01-01 20:08:08 +0530

    fix: css class and positioning issues in page
    Signed-off-by: Avinal Kumar <avinal.xlvii@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./layouts/**/*.html", "./public/**/*.html"],
+  content: [
+    "./layouts/**/*.html",
+    "./assets/**/*.html", 
+    "./public/**/*.html"
+  ],
   theme: {
     extend: {},
   },
